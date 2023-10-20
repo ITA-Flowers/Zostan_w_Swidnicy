1b66a7141ddfe669102855818e3cf4d3aaa79f2f
Baro-coder <b.szykula00@gmail.com>
2023-10-20 12:42:29 +0200

    Inicjalizacja modułu usługi Identity : podstawowy interfejs

---
+++ software/services/identity/app/api/__meta.py
@@ -0,0 +1,19 @@
+TITLE = "Identity Service"
+
+DESCRIPTION = """
+
+### #Zostań_w_Świdnicy - moduł API mikrousługi *Identity*.
+
+Usługa implementuje i udostępnia interfejs do operacji logowania i rejestracji użytkowników oraz 
+podstawowych operacji utwardzających działanie platformy takich jak tokeny JWT.
+
+"""
+
+VERSION = "1.0.0"
+
+TAGS_METADATA = [    
+    {
+        "name" : "Identity",
+        "description" : "Rejestracja nowych użytkowników oraz uwierzytelnianie i autoryzowanie zarejestrowanych"
+    },
+]
---
+++ software/services/identity/app/api/models/__init__.py
@@ -0,0 +1,2 @@
+from .requests import LoginRequestModel, RegisterRequestModel
+from .responses import ResponseModel, ErrorResponseModel, create_response, create_error_response
