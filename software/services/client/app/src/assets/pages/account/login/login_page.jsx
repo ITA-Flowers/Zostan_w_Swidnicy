bde25b888d669c3f6496f330bf3aa1110fbcc5e9
PhilipMich <f.szpregiel@gmail.com>
2023-10-20 15:55:00 +0200

    Dzialajca walidacja oraz popup o wyslaniu maila z potwierdzeniem

--- software/services/client/app/src/assets/pages/account/login/login_page.jsx
+++ software/services/client/app/src/assets/pages/account/login/login_page.jsx
@@ -3,16 +3,31 @@ import user_icon from './assets/person.png';
 import email_icon from './assets/email.png';
 import password_icon from './assets/password.png';
 import { useState } from 'react';
-
+import InfoModal from './Popup';
+ 
 export default function Login() {
     const [action, setAction] = useState("Zaloguj się");
     const initialValues = {
         email: "",
         password: "",
-        confirm_password: ""
+        confirm_password: "",
+        nip: ""
     };
     const [formValues, setFormValues] = useState(initialValues);
     const [formErrors, setFormErrors] = useState({});
+    // dla NIP
+    const [isChecked, setIsChecked] = useState(false);
+    // do powiadomienia o wyslaniu maial
+    const [isModelOpen, setIsModelOpen] = useState(false);
+
+    const openModel = () => {
+        setIsModelOpen(true)
+    }
+
+    const closeModel = () => {
+        setIsModelOpen(false)
+    }
+   
 
     const handleChange = (e) => {
         // console.log(e.target)
@@ -21,18 +36,27 @@ export default function Login() {
         // console.log(formValues)
     };
 
-    const handleSubmit = (e) => {
-        if (action == "Zaloguj się"){
-            setAction("Zarejestruj się");
-        } else {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setFormErrors(validate(formValues));
-        if ( Object.keys(formErrors).length === 0){
-            // tu bedzie akcja jak bedzie logowanie api dzialalo
-            console.log("giit");
+    
+        if (action === "Zaloguj się") {
+            setActionAndResetForm("Zarejestruj się");
+        } else {
+            const errors = validate(formValues);
+            setFormErrors(errors);
+    
+            if (Object.keys(errors).length === 0) {
+                
+                openModel();
+    
+               
+                resetForm();
+            } else {
+                // Wywołaj to tylko, gdy są błędy
+                
+            }
         }
-    }
-    }
+    };
 
     const validate = (values) => {
         const errors = {}
@@ -52,15 +76,34 @@ export default function Login() {
             
             errors.email = "Uzupełnij wszystkie pola";
         }
+        if(isChecked && !values.nip){
+            errors.nip = "Uzupełnij NIP"
+        }
         
         
         return errors;
 
     }
 
+    
+    // czyszczenie inputow
+    const resetForm = () => {
+        setFormValues(initialValues);
+        setFormErrors({});
+    }
+
+    const setActionAndResetForm = (newAction) => {
+        setAction(newAction);
+        resetForm();
+    }
+    
+
     return (
         <div className='container'>
-            
+            <InfoModal
+                isOpen={isModelOpen}
+                closeModal={closeModel}
+                message="Mail z potwierdzeniem został wysłany na podany adres"></InfoModal>
             <div className="header">
                 <div className="text">
                     {action}
@@ -70,20 +113,61 @@ export default function Login() {
             <div className="inputs">
                 <div className="input">
                     <img src={email_icon} alt="" />
-                    <input onChange={handleChange} value={formValues.email} name='email' type="email" placeholder='Podaj swój adres email' />
+                    <input 
+                    onChange={handleChange} 
+                    value={formValues.email} 
+                    name='email' type="email" 
+                    placeholder='Podaj swój adres email' />
                 </div>
                 {action === "Zarejestruj się" ? <p className='errors'>{formErrors.email}</p> : null}
                 <div className="input">
                     <img src={password_icon} alt="" />
-                    <input onChange={handleChange} value={formValues.password} name='password' type="password" placeholder='Podaj hasło' />
+                    <input 
+                    onChange={handleChange} 
+                    value={formValues.password} 
+                    name='password' 
+                    type="password" 
+                    placeholder='Podaj hasło' />
                 </div>
                 {action === "Zarejestruj się" ? <p className='errors'>{formErrors.password}</p> : null}
                 {action === "Zaloguj się" ? null : (
                     <div className="input">
                         <img src={password_icon} alt="" />
-                        <input onChange={handleChange} value={formValues.confirm_password} name='confirm_password' type="password" placeholder='Podaj ponownie hasło' />
+                        <input 
+                        onChange={handleChange} 
+                        value={formValues.confirm_password} 
+                        name='confirm_password' 
+                        type="password" 
+                        placeholder='Podaj ponownie hasło' />
                     </div>
                 )}
+                {action==="Zarejestruj się" ?  
+                <div  >
+                    <label className='checkbox-label'>
+                        <input 
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={()=> setIsChecked(!isChecked)}
+                            /> Rejestrujesz się jako firma?
+                    </label>
+                    <br/>
+                    
+                    {isChecked && (
+                         <div className="input">
+                         <img src={user_icon} alt="" />
+                         <input
+                             onChange={handleChange}
+                             value={formValues.nip}
+                             name='nip'
+                             type="text"
+                             placeholder='Podaj NIP'
+                         />
+                        {action === "Zarejestruj się" ? <p className='errors'>{formErrors.nip}</p> : null}     
+                     </div>
+                     
+                    )}
+                </div> : null}
+               
             </div>
             {action === "Zarejestruj się" ? <p className='errors'>{formErrors.confirm_password}</p> : (
                 <div className="forgot-password">
@@ -94,7 +178,7 @@ export default function Login() {
                 <div className={action === "Zaloguj się" ? "submit grey" : "submit"} onClick={ handleSubmit}>
                     Zarejestruj się
                 </div>
-                <div className={action === "Zarejestruj się" ? "submit grey" : "submit"} onClick={() => {setAction("Zaloguj się");}}>
+                <div className={action === "Zarejestruj się" ? "submit grey" : "submit"} onClick={() => {setActionAndResetForm("Zaloguj się");}}>
                     Zaloguj się
                 </div>
             </div>
