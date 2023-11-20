9372bba31acc6faf4123b8727d274323fd25e20a
Walu064 <waluwalu064@gmail.com>
2023-11-20 08:28:43 +0100

    Requirements for employee service

---
+++ software/services/employee/app/models/__init__.py
@@ -0,0 +1,2 @@
+from .user import User
+from .cv import Cv
--- software/services/employee/app/routers/modification.py
+++ software/services/employee/app/routers/modification.py
@@ -1,12 +1,2 @@
-from fastapi import APIRouter
+from models import User, Cv
 from app.models.user import User
-
-router = APIRouter()
-
-@router.post("/user", response_model = User)
-def user_modify(user : User):
-    # TODO: Strzał na DB Driver Service z nowymi danymi użytkownika.
-
-@router.post("/cv", response_model = Cv)
-def cv_modify(cv : Cv, user : User):
-    # TODO: Obsługa przyjęcia danych do CV, strzał na DB Driver Service
