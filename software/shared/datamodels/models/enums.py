48ba1abf2aa1483a954e13892c941d2cff22b5c6
Baro-coder <b.szykula00@gmail.com>
2023-12-05 08:21:21 +0100

    Shared libs update
    Shared datamodels update

---
+++ software/shared/datamodels/models/enums.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+# Enum dla working_time w tabeli Jobs
+class WorkingTimeEnum(str, Enum):
+    full_time = 'full-time'
+    part_time = 'part-time'
+    temporary = 'temporary'
+
+# Enum dla contract_type w tabeli Jobs
+class ContractTypeEnum(str, Enum):
+    employment = 'employment'
+    contract = 'contract'
+    task_specific = 'task-specific'
+    internship = 'internship'
+    b2b = 'b2b'
+
+# Enum dla work_type w tabeli Jobs
+class WorkTypeEnum(str, Enum):
+    stationary = 'stationary'
+    hybrid = 'hybrid'
+    remote = 'remote'
+    mobile = 'mobile'
+
+# Enum dla level w tabeli ReportEducations
+class EducationLevelEnum(str, Enum):
+    primary = 'primary'
+    secondary = 'secondary'
+    bachelor = 'bachelor'
+    master = 'master'
+    doctorate = 'doctorate'
+    vocational = 'vocational'
+    continuing = 'continuing'
